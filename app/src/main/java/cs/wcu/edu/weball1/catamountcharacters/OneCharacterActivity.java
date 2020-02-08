603cda4a8254baeb6e26048a3636925a95307e7e
Evert Ball <weball1@catamount.wcu.edu>
2020-02-08 17:35:29 -0500

    Added input validation helper method and javadocs.

--- app/src/main/java/cs/wcu/edu/weball1/catamountcharacters/OneCharacterActivity.java
+++ app/src/main/java/cs/wcu/edu/weball1/catamountcharacters/OneCharacterActivity.java
@@ -9,10 +9,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/**
+ * Activity where the user is asked to supply a single character to be displayed
+ * as an image on the screen.
+ *
+ * @author Evert Ball
+ * @version 20 February 2020
+ *
+ */
 public class OneCharacterActivity extends AppCompatActivity {
 
+    private static final String NON_ALPHANUMERIC_ERROR_MSG = "Only single alphanumeric characters allowed.";
     EditText editOneChar;
 
+    /**
+     * Called when the activity is starting. All initialization is done within
+     * this method, as well as programmatically retrieving UI elements.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle
+     *                           contains the data it most recently supplied in
+     *                           onSaveInstanceState(Bundle).
+     *                           This value may be null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,19 +40,49 @@ public class OneCharacterActivity extends AppCompatActivity {
         this.editOneChar = this.findViewById(R.id.edit_one_char);
     }
 
-    public void goBtnClicked(View v) {
+    /**
+     * The action that is performed upon the user clicking the 'Go >' button on
+     * the OneCharacterActivity screen.
+     * @param view The element that has been clicked. Not used in this function,
+     *             but is required by parent class.
+     */
+    public void goBtnClicked(View view) {
+
         String character = this.editOneChar.getText().toString().toLowerCase().trim();
-        if(character.length() > 1) {
-            Toast.makeText(getApplicationContext(),
-                    "Only 1 alphanumeric character allowed. You entered: " + character,
-                    Toast.LENGTH_LONG).show();
-            return;
-        } else if(character.matches("\\d+")) {
+        boolean valid = isValidInput(character);
+
+        if(character.matches("\\d")) {
             character = "num_" + character;
         }
-        Intent displaySingleActivity = new Intent(this, DisplaySingleImageActivity.class);
-        displaySingleActivity.putExtra("to_display", character + "_bl");
-        startActivity(displaySingleActivity);
-    }
 
-}
+        if(valid) {
+            Intent displaySingleActivity = new Intent(this, DisplaySingleImageActivity.class);
+            displaySingleActivity.putExtra("to_display", character + "_bl");
+            startActivity(displaySingleActivity);
+        } // end if statement
+    } // end goBtnClicked method
+
+    /**
+     * Helper method that validates user input.
+     *
+     * @param input The user input from the EditText
+     * @return Returns true if user input is a single alphanumeric character,
+     *         false otherwise.
+     */
+    private boolean isValidInput(String input) {
+
+        boolean valid = false;
+        String alphanumeric = "\\d|\\w";
+
+        if(!input.matches(alphanumeric)) {
+            Toast.makeText(getApplicationContext(),
+                    NON_ALPHANUMERIC_ERROR_MSG,
+                    Toast.LENGTH_LONG).show();
+        } else {
+            valid = true;
+        } // end if-else statement
+
+        return valid;
+    } // end isValidInput method
+
+} // end OneCharacterActivity class
